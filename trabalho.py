71b1f7d98ce13c01adc7c53b42c6d83af39964e6
LucasGazetta <gazetinhagames@gmail.com>
2021-08-02 15:59:42 -0300

    média e estilização

--- trabalho.py
+++ trabalho.py
@@ -37,9 +37,19 @@ ws.title = "Notas"
 headings = ['Nome'] + list(data['João'].keys())
 ws.append(headings)
 
+#preenche as cédulas com as notas e os nomes dos alunos
 for pessoa in data:
     notas = list(data[pessoa].values())
     ws.append([pessoa] + notas)
+
+#faz a média das notas
+for col in range(2, len(data['João']) + 2):
+    char = get_column_letter(col)
+    ws[char + "7"] = f"=SUM({char + '2'}:{char + '6'})/{len(data)}"
+
+#estiliza as cédulas
+for col in range(1,6):
+    ws[get_column_letter(col) + '1'].font = Font(bold=True, color="0099CCFF")
 
 
 wb.save("NovoNotas.xlsx")
